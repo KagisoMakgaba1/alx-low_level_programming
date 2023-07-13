f82ce613a85e3d864dc97c848f7ef0b0cb5d3d05
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2023-07-13 02:26:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
   *main - prints "Programming is like building a multilingual puzzle"
-  *return: always 0 (success)
+  *Return: 0 if exited properly, non-zero otherwise
   */
 int main(void)
 {
