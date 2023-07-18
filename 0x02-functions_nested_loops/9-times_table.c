29729dec3ac987e6ae6a047d70a0cdb8dea4d0cb
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2023-07-18 09:49:23 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,6 +13,11 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			res = (i * j);
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			if (res >= 10)
 			{
 				_putchar((res / 10) + '0');
