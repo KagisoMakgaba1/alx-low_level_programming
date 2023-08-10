c40814a63254649cffb6bb7f201820b118b76bd9
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2023-08-10 12:41:38 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,36 +2,52 @@
 #include <stdio.h>
 
 /**
+*is_digit - checks if string contains digits
+*@str: source string
+*Return: 1 if success, 0 otherwise
+*/
+
+int is_digit(char *str)
+{
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+		{
+			return (0);
+		}
+		str++;
+	}
+	return (1);
+}
+
+/**
 * main - multiply 2 numbers as arguments
 * @argc: number of arguments
 * @argv: argument vector
-* Return: 0 success
+* Return: 0 on success
 */
 
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
+	unsigned long int num1, num2, result;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
-	for (i = 1; i < argc; i++)
+
+	if (!is_digit(argv[1]) || !is_digit(argv[2]))
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
+		printf("Error\n");
+		return (98);
 	}
 
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
+	num1 = strtoull(argv[1], NULL, 10);
+	num2 = strtoull(argv[2], NULL, 10);
+	result = num1 * num2;
 
-return (0);
+	printf("%lu\n", result);
+
+	return (0);
 }
