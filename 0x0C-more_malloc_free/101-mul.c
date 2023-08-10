9319c97981f0495b0d60fd581735d63af21440b3
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2023-08-10 16:15:49 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,15 +1,21 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
-*is_digit - checks if string contains digits
-*@str: source string
-*Return: 1 if success, 0 otherwise
+* is_positive_integer - checks if a string is a positive integer
+* @str: input string
+* Return: 1 if positive integer, 0 otherwise
 */
 
-int is_digit(char *str)
+int is_positive_integer(char *str)
 {
-	while (*str)
+	if (*str == '\0')
+	{
+		return (0);
+	}
+
+	while (*str != '\0')
 	{
 		if (*str < '0' || *str > '9')
 		{
@@ -17,19 +23,55 @@ int is_digit(char *str)
 		}
 		str++;
 	}
+
 	return (1);
 }
 
 /**
-* main - multiply 2 numbers as arguments
-* @argc: number of arguments
-* @argv: argument vector
-* Return: 0 on success
+* multiply_strings - multiplies two strings representing positive integers
+* @str1: first input string
+* @str2: second input string
+* @result: buffer to store the result
+* @result_size: size of the result buffer
+*/
+
+void multiply_strings(char *str1, char *str2, char *result, size_t result_size)
+{
+	int i, j;
+	size_t len1 = strlen(str1);
+	size_t len2 = strlen(str2);
+
+	memset(result, '0', result_size);
+
+	for (i = len2 - 1; i >= 0; i--)
+	{
+		int carry = 0;
+
+		for (j = len1 - 1; j >= 0; j--)
+		{
+			int product = (str1[j] - '0') * (str2[i] - '0') + (result[i + j + 1] - '0')
+				+ carry;
+
+			carry = product / 10;
+
+			result[i + j + 1] = (product % 10) + '0';
+		}
+
+		result[i] = carry + '0';
+	}
+}
+
+/**
+*main - main function
+*@argc: argument count
+*@argv: argument vector
+*Return: 0 on success
 */
 
 int main(int argc, char *argv[])
 {
-	unsigned long int num1, num2, result;
+	size_t result_size;
+	char *result, *trimmed_result, *num1 = argv[1], *num2 = argv[2];
 
 	if (argc != 3)
 	{
@@ -37,17 +79,33 @@ int main(int argc, char *argv[])
 		return (98);
 	}
 
-	if (!is_digit(argv[1]) || !is_digit(argv[2]))
+	if (!is_positive_integer(argv[1]) || !is_positive_integer(argv[2]))
 	{
 		printf("Error\n");
 		return (98);
 	}
 
-	num1 = strtoull(argv[1], NULL, 10);
-	num2 = strtoull(argv[2], NULL, 10);
-	result = num1 * num2;
+	result_size = strlen(num1) + strlen(num2) + 1;
+	result = malloc(result_size);
 
-	printf("%lu\n", result);
+	if (result == NULL)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	multiply_strings(num1, num2, result, result_size);
+
+	trimmed_result = result;
+
+	while (*trimmed_result == '0' && *(trimmed_result + 1) != '\0')
+	{
+		trimmed_result++;
+	}
+
+	printf("%s\n", trimmed_result);
+
+	free(result);
 
 	return (0);
 }
