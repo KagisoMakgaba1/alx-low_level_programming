82cf32f12b5ab35503d323788a64d0670c4bef34
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2023-08-10 07:27:26 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,23 +11,19 @@
 
 int *array_range(int min, int max)
 {
-	int *ptr;
-	int i, j = 0;
+	int *newArray, *temp;
 
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(sizeof(ptr) * ((max - min) + 1));
+	newArray = malloc((max - min + 1) * sizeof(int));
 
-	if (ptr != NULL)
-	{
-		for (i = min; i <= max; i++)
-		{
-			ptr[j] = i;
-			j++;
-		}
-		return (ptr);
-	}
-	else
+	if (!newArray)
 		return (NULL);
+
+	temp = newArray;
+	while (min <= max)
+		*temp++ = min++;
+
+return (newArray);
 }
