f2dfc915d80510a7bfb19ff579530abd124fb3f6
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2023-10-02 05:56:58 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	size_t n = 0;
+	ssize_t p = 0;
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	for (n = 0; text_content && text_content[n]; n++)
+		;
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (n)
+		p = write(fd, text_content, n);
+	close(fd);
+
+	if (p == -1)
+		return (-1);
+	return (1);
+}
