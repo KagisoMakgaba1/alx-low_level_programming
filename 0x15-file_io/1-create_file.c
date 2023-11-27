d1d73525f2142a726f259f392fd6237033967c1d
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2023-11-27 09:27:55 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,8 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
  * create_file - creates a file
- * @filename: name of the file to create
+ * @filename: name of file to create
  * @text_content: a NULL terminated string to write to the file
  *
  * Return: 1 on success, -1 on failure
@@ -22,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	for (n = 0; text_content && text_content[n]; n++)
 		;
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY | S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
