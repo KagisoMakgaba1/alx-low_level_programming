bf6723e04f15d1ea1fdfe7958c8f4b215412fd5e
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-01-15 10:37:41 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/gigalib.so https://raw.githubusercontent.com/KagisoMakgaba1/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gigalib.so
-export LD_PRELOAD=/tmp/gigalib.so
+wget -q -O /tmp/libinject.so https://raw.githubusercontent.com/KagisoMakgaba1/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libinject.so
+export LD_PRELOAD=/tmp/libinject.so
