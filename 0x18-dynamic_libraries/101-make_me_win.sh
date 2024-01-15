d154a4420e7820e528867983cf1c7e464996c05d
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-01-15 09:51:07 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/KagisoMakgaba1/alx-low_level_programming/blob/master/libhack.so
+wget -P .. https://raw.github.com/KagisoMakgaba1/alx-low_level_programming/blob/master/libhack.so
 export LD_PRELOAD="$PWD/../libhack.so"
