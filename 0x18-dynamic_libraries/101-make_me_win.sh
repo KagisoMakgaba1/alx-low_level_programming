64aff65890f3217d9cbb1e57a90c779820458781
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-01-15 09:45:54 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.github.com/KagisoMakgaba1/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libinject.so
-export LD_PRELOAD="$PWD/../libinject.so"
+wget -P .. https://raw.github.com/KagisoMakgaba1/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
