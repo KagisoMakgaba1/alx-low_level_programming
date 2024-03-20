d2f88e936807b5c938f286b5afe070b6790e7e77
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-03-20 14:06:28 -0700

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""This module attempts to solve the Island Perimeter coding challenge."""
+
+def island_perimeter(grid: "list[list]") -> int:
+    """Calculates the perimeter of an island.
+
+    Args:
+        grid (list[list]): a grid containing the binary matrix.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    try:
+        rows, cols = len(grid), len(grid[0])
+    except IndexError:
+        return 0
+
+    perimeter = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
